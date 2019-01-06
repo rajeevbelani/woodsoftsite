50a90ac1f7611d999b1ebe582f3e961d72c298b7
Rajeev Belani <rajeev@woodsofttechnologies.com>
2019-01-06 15:07:06 +0530

    Changed Footer

--- src/components/wssite/footer/footer.js
+++ src/components/wssite/footer/footer.js
@@ -4,92 +4,43 @@ import { Link } from 'gatsby';
 import { FaFacebook, FaTwitter } from 'react-icons/fa';
 // import posed from 'react-pose';
 
-const Footer = ({ title }) => (
-    <footer id="dark-footer" class="footer footer-dark">
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <div class="footer-column">
-                        <div class="footer-header">
-                            <h3>Product</h3>
-                        </div>
-                        {/* <ul class="link-list">
-                            <li><a href="#">Discover features</a></li>
-                            <li><a href="#">CMS integration</a></li>
-                            <li><a href="#">Customers</a></li>
-                            <li><a href="#">Weekly sessions</a></li>
-                            <li><a href="#">Free trials and demo</a></li>
-                            <li><a href="#">What's next ?</a></li>
-                        </ul> */}
+const Footer = () => (
+    <footer class="footer colored-footer">
+        {/* <div class="container">
+            
+            <div class="footer-body">
+                <div class="footer-form">
+            
+                    <img src="assets/images/logos/bulkit-white.svg" alt="" />
+            
+                    <div class="control">
+                        <input type="text" class="input is-rounded" placeholder="Enter your email" />
+                        <button type="button" class="button input-button">Sign up</button>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="footer-column">
-                        <div class="footer-header">
-                            <h3>Company</h3>
-                        </div>
-                        {/* <ul class="link-list">
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">About security</a></li>
-                            <li><a href="#">User guide</a></li>
-                            <li><a href="#">Terms of service</a></li>
-                            <li><a href="#">Terms of website use</a></li>
-                            <li><a href="#">Privacy policy</a></li>
-                        </ul> */}
+            </div>
+        </div> */}
+        
+        <div class="footer-footer">
+            <div class="container">
+                <div class="footer-inner">
+                    <div class="footer-block designed-by">
+                        © 2018 Woodsoft Technologies
                     </div>
-                </div>
-                <div class="column">
-                    <div class="footer-column">
-                        <div class="footer-header">
-                            <h3>Learning</h3>
-                        </div>
-                        {/* <ul class="link-list">
-                            <li><a href="#">Resources</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">API documentation</a></li>
-                            <li><a href="#">Admin guide</a></li>
-                        </ul> */}
+                    <div class="footer-block links has-text-centered">
+                        <a href="#">Terms &amp; Conditions</a>
+                        <a href="#">Privacy policy</a>
+                        <a href="#">Help</a>
                     </div>
-                </div>
-                <div class="column">
-                    <div class="footer-column">
-                        <div class="footer-logo">
-                            <img src="assets/images/logos/bulkit-green-w.svg" alt="" />
-                        </div>
-                        <div class="footer-header">
-                            <nav class="level is-mobile">
-                                <div class="level-left level-social">
-                                    <a href="#" class="level-item">
-                                        <span class="icon"><FaFacebook/></span>
-                                    </a>
-                                    <a href="#" class="level-item">
-                                        <span class="icon"><i class="fa fa-twitter"></i></span>
-                                    </a>
-                                    <a href="#" class="level-item">
-                                        <span class="icon"><i class="fa fa-linkedin"></i></span>
-                                    </a>
-                                    <a href="#" class="level-item">
-                                        <span class="icon"><i class="fa fa-dribbble"></i></span>
-                                    </a>
-                                    <a href="#" class="level-item">
-                                        <span class="icon"><i class="fa fa-github"></i></span>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                        <div class="copyright">
-                            <span class="moto light-text">Designed and coded with <i class="fa fa-heart color-red"></i> by CSS Ninja.</span>
-                        </div>
+                    <div class="footer-block social has-text-right">
+                        {/* <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a> */}
                     </div>
                 </div>
             </div>
         </div>
     </footer>
 );
-
-Footer.propTypes = {
-  title: PropTypes.string.isRequired,
-};
 
 export default Footer;
