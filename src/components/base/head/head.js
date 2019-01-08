67f24dadea923a83ab1698036b67d1c011e780a4
Rajeev Belani <rajeev@woodsofttechnologies.com>
2019-01-08 13:31:32 +0530

    Favicon URL

--- src/components/base/head/head.js
+++ src/components/base/head/head.js
@@ -66,73 +66,73 @@ const Head = ({
     <meta content="512" property="og:image:height" />
 
     <meta content={themeColor} name="msapplication-TileColor" />
-    <meta content="/icons/mstile-70x70.png" name="msapplication-square70x70" />
+    <meta content="/assets/icons/mstile-70x70.png" name="msapplication-square70x70" />
     <meta
-      content="/icons/mstile-144x144.png"
+      content="/assets/icons/mstile-144x144.png"
       name="msapplication-square144x144"
     />
     <meta
-      content="/icons/mstile-150x150.png"
+      content="/assets/icons/mstile-150x150.png"
       name="msapplication-square150x150"
     />
     <meta
-      content="/icons/mstile-310x150.png"
+      content="/assets/icons/mstile-310x150.png"
       name="msapplication-wide310x150"
     />
     <meta
-      content="/icons/mstile-310x310.png"
+      content="/assets/icons/mstile-310x310.png"
       name="msapplication-square310x310"
     />
 
     <link href="/manifest.json" rel="manifest" />
 
     <link
-      href="/icons/apple-icon-57x57.png"
+      href="/assets/icons/apple-icon-57x57.png"
       rel="apple-touch-icon"
       sizes="57x57"
     />
     <link
-      href="/icons/apple-icon-60x60.png"
+      href="/assets/icons/apple-icon-60x60.png"
       rel="apple-touch-icon"
       sizes="60x60"
     />
     <link
-      href="/icons/apple-icon-72x72.png"
+      href="/assets/icons/apple-icon-72x72.png"
       rel="apple-touch-icon"
       sizes="72x72"
     />
     <link
-      href="/icons/apple-icon-76x76.png"
+      href="/assets/icons/apple-icon-76x76.png"
       rel="apple-touch-icon"
       sizes="76x76"
     />
     <link
-      href="/icons/apple-icon-114x114.png"
+      href="/assets/icons/apple-icon-114x114.png"
       rel="apple-touch-icon"
       sizes="114x114"
     />
     <link
-      href="/icons/apple-icon-120x120.png"
+      href="/assets/icons/apple-icon-120x120.png"
       rel="apple-touch-icon"
       sizes="120x120"
     />
     <link
-      href="/icons/apple-icon-144x144.png"
+      href="/assets/icons/apple-icon-144x144.png"
       rel="apple-touch-icon"
       sizes="144x144"
     />
     <link
-      href="/icons/apple-icon-152x152.png"
+      href="/assets/icons/apple-icon-152x152.png"
       rel="apple-touch-icon"
       sizes="152x152"
     />
     <link
-      href="/icons/apple-icon-167x167.png"
+      href="/assets/icons/apple-icon-167x167.png"
       rel="apple-touch-icon"
       sizes="167x167"
     />
     <link
-      href="/icons/apple-icon-180x180.png"
+      href="/assets/icons/apple-icon-180x180.png"
       rel="icon"
       sizes="180x180"
       type="image/png"
@@ -145,7 +145,7 @@ const Head = ({
       type="image/png"
     />
     <link
-      href="/assets/favicon-16x16.png"
+      href="/assets/icons/favicon-16x16.png"
       rel="icon"
       sizes="16x16"
       type="image/png"
